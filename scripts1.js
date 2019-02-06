f0204f72c34b1657833608b8884170775988a235
strawhare <1121hatsuyuki@gmail.com>
2019-02-06 16:27:27 +0900

    wall 初めから表示されるように

--- scripts1.js
+++ scripts1.js
@@ -2,7 +2,8 @@ var under;
 var num = 0;
 window.onload = function(){
     under = document.getElementById('first');
-    append();
+    appendWallInit();
+    appendWall();
     createAlice();
 }
 
@@ -49,7 +50,7 @@ function rect() {
     setTimeout('rect()', 3200);
 }
 
-function append() {
+function appendWall() {
     //動的にDiv要素を生成する
     var div = document.createElement('div');
     div.setAttribute("id", `${num}`);
@@ -81,8 +82,79 @@ function append() {
     first.appendChild(div);
     up();
     num += 1;
-    setTimeout('append()',1800);
+    setTimeout('appendWall()',1800);
 };
+
+function appendWallInit() {
+    var div = document.createElement('div');
+    div.setAttribute("id", `-1`);
+    div.style.zIndex = -1
+    var h = window.innerHeight;
+    var w = window.innerWidth;
+
+    div.style.backgroundColor = "white";
+    div.style.position = "absolute";
+    div.style.top = h - 300 + "px";
+    div.style.height = h + "px";
+    div.style.right = w/10 + "px";
+    div.style.width = w*4/5 + "px";
+    var rightImg = document.createElement('img');
+    rightImg.style.position = "absolute";
+    rightImg.src = "images/wall_right.png";
+    rightImg.style.height = h + "px";
+    rightImg.style.right = 0 + "px";
+    //Divにイメージを組み込む
+    div.appendChild(rightImg);
+    var leftImg = document.createElement('img');
+    leftImg.style.position = "absolute";
+    leftImg.src = "images/wall_left.png";
+    leftImg.style.height = h + "px";
+    leftImg.style.left = 0 + "px";
+    //Divにイメージを組み込む
+    div.appendChild(leftImg);
+    //Divを組み込む
+    first.appendChild(div);
+    $(`#-1`).animate({
+        "top": "-=" + h*2 + "px"
+    }, 6000,"linear").queue(function() {
+        this.remove();
+    });
+
+
+    var div = document.createElement('div');
+    div.setAttribute("id", `0`);
+    div.style.zIndex = -1
+    var h = window.innerHeight;
+    var w = window.innerWidth;
+
+    div.style.backgroundColor = "white";
+    div.style.position = "absolute";
+    div.style.top = 0 + "px";
+    div.style.height = h + "px";
+    div.style.right = w/10 + "px";
+    div.style.width = w*4/5 + "px";
+    var rightImg = document.createElement('img');
+    rightImg.style.position = "absolute";
+    rightImg.src = "images/wall_right.png";
+    rightImg.style.height = h + "px";
+    rightImg.style.right = 0 + "px";
+    //Divにイメージを組み込む
+    div.appendChild(rightImg);
+    var leftImg = document.createElement('img');
+    leftImg.style.position = "absolute";
+    leftImg.src = "images/wall_left.png";
+    leftImg.style.height = h + "px";
+    leftImg.style.left = 0 + "px";
+    //Divにイメージを組み込む
+    div.appendChild(leftImg);
+    //Divを組み込む
+    first.appendChild(div);
+    $(`#0`).animate({
+        "top": "-=" + h*2 + "px"
+    }, 6000,"linear").queue(function() {
+        this.remove();
+    });
+}
 
 function up() {
     var divH = window.innerHeight;
